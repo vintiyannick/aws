f99eb3fa0ef3e15beb0d8d0769f97cb9d4533365
vintiyannick <yannickvinti@gmail.com>
2025-05-07 11:31:36 +0200

    Wetterstationen Popup erstellt

--- main.js
+++ main.js
@@ -38,7 +38,27 @@ async function loadStations(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
 
-    // Wetterstationen mit Icons und Popups
+// Wetterstationen mit Icons und Popups
+    L.geoJSON(jsondata, {
+        attribution: "Datenquelle: <a href='https://static.avalanche.report/weather_stations/stations.geojson'> Wetterstationen</a>",
+        pointToLayer: function(feature, latlng) {
+            // console.log(feature.properties);
 
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: "icons/wifi.png",
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -16]
+                })
+            });
+        },
+        onEachFeature: function(feature, layer) {
+             //console.log(feature.properties);
+             layer.bindPopup(`
+                <h4>${feature.properties.name}</h4>
+                ${feature.properties.hoehe}
+                `);
+        }
+    }).addTo(overlays.stations);
 }
 loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
